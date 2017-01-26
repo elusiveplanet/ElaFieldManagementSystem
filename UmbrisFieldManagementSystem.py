8b6ef39b3074f030b0750016255d96ba4f506f9f
HP ProBook <bearbotics2358@gmail.com>
2017-01-26 09:57:31 -0600

    Pushing some random code just to test Sourcetree features. Nothing notable. -Alexis

--- UmbrisFieldManagementSystem.py
+++ UmbrisFieldManagementSystem.py
@@ -1,5 +1,7 @@
 import random, os.path
-import sys, pygame
+import sys
+import pygame
+from pygame.locals import *
 
 size = width, height = 720, 480
 speed = [2,2]
@@ -11,13 +13,27 @@ scoreBoard = pygame.image.load("data\\ScoreBack.png")
 
 boardRect = scoreBoard.get_rect()
 
+pygame.font.init()
+
+class Score(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.Font(None, 20)
+        self.font.set_italic(1)
+        self.color = Color('white')
+        self.lastscore = -1
+        self.update()
+        self.rect = self.image.get_rect().move(10, 450)
+
+    def update(self):
+        if SCORE != self.lastscore:
+            self.lastscore = SCORE
+            msg = "Score: %d" % SCORE
+            self.image = self.font.render(msg, 0, self.color)
+
 def main(winstyle = 0):
 	pygame.init()
 
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
-
-	boardRect = boardRect.move(speed)
-
-	screen.fill(black)
