19732567ac2084a587a90eebe5d05626dd98952e
Kurama <renderosalexis17@gmail.com>
2018-03-27 16:54:14 -0500

    Well, I had an error in that repo for the longest time. -Alexis

--- FMS/FMS.ino
+++ FMS/FMS.ino
@@ -114,13 +114,3 @@ void tickBeg() {
   digitalWrite(redPin, LOW);
   delay(1000);
 }
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
